a5bf7067b90876e503de09dfe6437747cfc3e0dc
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-10-24 16:23:04 -0400

    fade in nav bar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import logo from "../images/better_logo.jpg";
 const Navbar = () => {
-      const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-      const toggleMobileMenu = () => {
-        setMobileMenuOpen(!mobileMenuOpen);
-      };
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
 
   return (
-    <nav className="flex h-20  relative w-full items-center justify-between px-5 shadow-md">
+    <nav className="relative flex  h-20 w-full items-center justify-between px-5 shadow-md">
       <img src={logo} className="h-4/5" />
       <div className="hidden space-x-4 md:block">
         <a
@@ -43,8 +43,8 @@ const Navbar = () => {
           type="button"
           className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
           aria-controls="mobile-menu"
-                  aria-expanded={mobileMenuOpen}
-                  onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+          aria-expanded={mobileMenuOpen}
+          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
         >
           <span className="absolute -inset-0.5"></span>
           <span className="sr-only">Open main menu</span>
@@ -88,17 +88,42 @@ const Navbar = () => {
           </svg>
         </button>
       </div>
-      {mobileMenuOpen && (
-         <div className="sm:hidden absolute right-0 top-20 bg-slate-800 rounded" id="mobile-menu ">
-    <div className="space-y-1 px-2 pb-3 pt-2 ">
-      {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-      <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-      <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-      <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-      <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
-    </div>
-  </div>
-      )}
+
+      <div
+        className={`absolute right-0 top-20 transform rounded bg-slate-800 opacity-0 transition-opacity ${
+          mobileMenuOpen ? " opacity-100" : "opacity-0"
+        } `}
+        id="mobile-menu "
+      >
+        <div className="space-y-1 px-2 pb-3 pt-2 ">
+          {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+          <a
+            href="#"
+            className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+            aria-current="page"
+          >
+            Dashboard
+          </a>
+          <a
+            href="#"
+            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+          >
+            Team
+          </a>
+          <a
+            href="#"
+            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+          >
+            Projects
+          </a>
+          <a
+            href="#"
+            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+          >
+            Calendar
+          </a>
+        </div>
+      </div>
     </nav>
   );
 };
