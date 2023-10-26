eeeac8c803c86d5ffebfd38f028fbbe79df22108
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-10-26 16:45:26 -0400

    added testomonial carousel

--- src/Components/Testimonials.tsx
+++ src/Components/Testimonials.tsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css"; // Import Swiper styles
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 const Testimonials = () => {
   return (
@@ -6,43 +11,123 @@ const Testimonials = () => {
       <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
       <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
       <div className="mx-auto max-w-2xl lg:max-w-4xl">
-        <img
-          className="mx-auto h-12"
-          src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg"
-          alt=""
-        />
-        <figure className="mt-10">
-          <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
-            <p>
-              “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-              expedita voluptas culpa sapiente alias molestiae. Numquam corrupti
-              in laborum sed rerum et corporis.”
-            </p>
-          </blockquote>
-          <figcaption className="mt-10">
-            <img
-              className="mx-auto h-10 w-10 rounded-full"
-              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
-            <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-              <div className="font-semibold text-gray-900">Judith Black</div>
-              <svg
-                viewBox="0 0 2 2"
-                width={3}
-                height={3}
-                aria-hidden="true"
-                className="fill-gray-900"
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          speed={900}
+          autoplay={{
+            delay: 4500,
+            disableOnInteraction: false,
+            stopOnLastSlide: true,
+            pauseOnMouseEnter: true,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          loop={true}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          //   onAutoplayTimeLeft={onAutoplayTimeLeft}
+          className="mySwiper"
               >
-                <circle cx={1} cy={1} r={1} />
-              </svg>
-              <div className="text-gray-600">CEO of Workcation</div>
-            </div>
-          </figcaption>
-        </figure>
+
+        <SwiperSlide>
+          <figure className="mt-10">
+            <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+              <p>
+                “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                expedita voluptas culpa sapiente alias molestiae. Numquam
+                corrupti in laborum sed rerum et corporis.”
+              </p>
+            </blockquote>
+            <figcaption className="mt-10">
+              <img
+                className="mx-auto h-10 w-10 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                <div className="font-semibold text-gray-900">Judith Black</div>
+                <svg
+                  viewBox="0 0 2 2"
+                  width={3}
+                  height={3}
+                  aria-hidden="true"
+                  className="fill-gray-900"
+                >
+                  <circle cx={1} cy={1} r={1} />
+                </svg>
+                <div className="text-gray-600">CEO of Workcation</div>
+              </div>
+            </figcaption>
+          </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+          <figure className="mt-10">
+            <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+              <p>
+                “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                expedita voluptas culpa sapiente alias molestiae. Numquam
+                corrupti in laborum sed rerum et corporis.”
+              </p>
+            </blockquote>
+            <figcaption className="mt-10">
+              <img
+                className="mx-auto h-10 w-10 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                <div className="font-semibold text-gray-900">Judith Black</div>
+                <svg
+                  viewBox="0 0 2 2"
+                  width={3}
+                  height={3}
+                  aria-hidden="true"
+                  className="fill-gray-900"
+                  >
+                  <circle cx={1} cy={1} r={1} />
+                </svg>
+                <div className="text-gray-600">CEO of Workcation</div>
+              </div>
+            </figcaption>
+          </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+          <figure className="mt-10">
+            <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+              <p>
+                “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                expedita voluptas culpa sapiente alias molestiae. Numquam
+                corrupti in laborum sed rerum et corporis.”
+              </p>
+            </blockquote>
+            <figcaption className="mt-10">
+              <img
+                className="mx-auto h-10 w-10 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                <div className="font-semibold text-gray-900">Judith Black</div>
+                <svg
+                  viewBox="0 0 2 2"
+                  width={3}
+                  height={3}
+                  aria-hidden="true"
+                  className="fill-gray-900"
+                >
+                  <circle cx={1} cy={1} r={1} />
+                </svg>
+                <div className="text-gray-600">CEO of Workcation</div>
+              </div>
+            </figcaption>
+          </figure>
+        </SwiperSlide>
+        </ Swiper >
       </div>
     </section>
   );
-}
+};
 
-export default Testimonials
+export default Testimonials;
