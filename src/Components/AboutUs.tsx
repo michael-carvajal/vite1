b3c45b174f534d7806bed81b55beec2ae4d1a47b
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-10-30 20:36:15 -0400

    about us style

--- src/Components/AboutUs.tsx
+++ src/Components/AboutUs.tsx
@@ -6,7 +6,7 @@ const AboutUs = () => {
       <h1 className="mb-10 text-center text-3xl font-bold md:mb-16 md:text-4xl">
         WELCOME TO MT. KISCO SMOKEHOUSE{" "}
       </h1>
-      <div className="flex flex-col items-center gap-2 font-semibold md:flex-row md:text-lg">
+      <div className="flex flex-col items-center gap-2 font-semibold md:flex-row md:text-lg py-8 px-10 rounded text-white bg-[#F37624]">
         <p>520 Lexington Ave</p>
         <svg
           viewBox="0 0 2 2"
