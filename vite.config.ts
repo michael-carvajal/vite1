3081c6fa2129a96f14f5a483d8f981a2a2570f53
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-11-27 19:52:41 -0500

    added babel core

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: "/vite1/",
+  plugins: [react()],
+  optimizeDeps: { include: ["swiper/react", "swiper/types"] },
+});
