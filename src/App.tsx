8cedb47d1f7a61ca9ecb9901c1122f1f971874e1
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-11-28 19:32:49 -0500

    able to tansition opacity on scoll to top button

--- src/App.tsx
+++ src/App.tsx
@@ -17,27 +17,10 @@ function App() {
       <Testimonials />
       <Footer />
       <ScrollToTop
-        smooth
+        smooth={true}
         color="white"
         component={<Arrow  />}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          position: "fixed",
-          bottom: "25px",
-          right: "25px",
-          borderRadius: "25%",
-          backgroundColor: "#FF4500", // Use your preferred orange color
-          padding: "8px",
-          fontSize: "12px",
-          fontWeight: "600",
-          textTransform: "uppercase",
-          color: "white",
-          boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-          transition: "background-color 0.3s, box-shadow 0.3s",
-        }}
-        // other props...
+        className="custom-scroll-top"
       />
     </div>
   );
